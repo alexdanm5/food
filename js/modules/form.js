f8325baaa70082e2c482846d677a7325785049e9
unknown <alex.dan.m5@gmail.com>
2026-02-27 10:03:05 +0200

    deleted not needed comments

--- js/modules/form.js
+++ js/modules/form.js
@@ -30,7 +30,6 @@ function form(formSelector, modalTaimerId) {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-// Более удобный способ оьправки с использованием дополнительной функции и операторами async await
             const formData = new FormData(form);
             const json =JSON.stringify(Object.fromEntries(formData.entries()));
 
@@ -45,82 +44,6 @@ function form(formSelector, modalTaimerId) {
             }).finally(() => {
                 form.reset();
             });
-
-            // Отправка с помощю feth простой formData
-            // const formData = new FormData(form);
-            // fetch('server.php', {
-            //     method: "POST",
-            //     body: formData
-            // }).then(data => data.text())
-            // .then(data => {
-            //     console.log(data);
-            //     showThinksModal(message.success);
-            //     statusMessage.remove()
-            // }).catch(() => {
-            //     showThinksModal(message.failure);
-            // }).finally(() => {
-            //     form.reset();
-            // });
-
-             // Отправка с помощю feth в JSON
-
-            // const formData = new FormData(form);
-            // const object = {};
-            // formData.forEach(function(value, key) {
-            //     object[key] = value;
-            // });
-
-            
-            // fetch('server.php', {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-type': 'application/json'
-            //     },
-            //     body: JSON.stringify(object)
-            // })
-            // .then(data => data.text())
-            // .then(data => {
-            //     console.log(data);
-            //     showThinksModal(message.success);
-            //     statusMessage.remove()
-            // }).catch(() => {
-            //     showThinksModal(message.failure);
-            // }).finally(() => {
-            //     form.reset();
-            // });
-            
-
-            // Отправка в формате JSON
-            // reqest.setRequestHeader('Content-type', 'application/json');
-            // const formData = new FormData(form);
-
-            // const object = {};
-            // formData.forEach(function(value, key) {
-            //     object[key] = value;
-            // });
-
-            // const json = JSON.stringify(object);
-            // reqest.send(json);
-
-            // Отправка в формате XMLHttpRequest
-            // const reqest = new XMLHttpRequest();
-            // reqest.open('POST', 'server.php');
-
-            // const formData = new FormData(form);
-            // reqest.send(formData);
-
-            // reqest.addEventListener('load', () => {
-            //     if (reqest.status === 200) {
-            //         console.log(reqest.response);
-            //         showThinksModal(message.success);
-            //         form.reset();
-                    
-            //         statusMessage.remove()
-                    
-            //     }else {
-            //         showThinksModal(message.failure);
-            //     }
-            // })
         })
     }
     function showThinksModal(message) {
